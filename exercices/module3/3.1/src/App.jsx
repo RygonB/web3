a87d942b92bf1acadeda07dd75d0941527228716
RygonB <rayane.belbachir@student.vinci.be>
2024-10-22 13:56:49 +0200

    finish 1.7

---
+++ exercices/module3/3.1/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+
+  const handleClickGood = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+  };
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  };
+  const handleClickBad = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+  };
+
+  const average = all !== 0 ? (good - bad) / all : 0;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <div>
+        <button onClick={handleClickGood}>good</button>
+        <button onClick={handleClickNeutral}>neutral</button>
+        <button onClick={handleClickBad}>bad</button>
+      </div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+    </div>
+  );
+};
+
+export default App;
